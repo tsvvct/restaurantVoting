356d8a893edab28c7cc0c1ad0aa4647cd092439a
tsv <victor.tsvikevich@gmail.com>
2022-01-08 15:17:45 +0300

    menuItem tests

---
+++ src/test/java/ru/javaops/topjava/web/menuitem/MenuItemControllerTest.java
@@ -0,0 +1,109 @@
+package ru.javaops.topjava.web.menuitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.javaops.topjava.model.MenuItem;
+import ru.javaops.topjava.repository.MenuItemRepository;
+import ru.javaops.topjava.util.JsonUtil;
+import ru.javaops.topjava.web.AbstractControllerTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javaops.topjava.web.menuitem.MenuItemTestData.*;
+import static ru.javaops.topjava.web.user.UserTestData.ADMIN_MAIL;
+import static ru.javaops.topjava.web.user.UserTestData.USER_MAIL;
+
+class MenuItemControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = MenuItemController.REST_URL + '/';
+
+    @Autowired
+    private MenuItemRepository menuItemRepository;
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + MENU_ITEM1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MENU_ITEM_MATCHER.contentJson(restaurant1_menuitem1));
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getForbidden() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + MENU_ITEM1_ID))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + MENU_ITEM1_ID))
+                .andExpect(status().isNoContent());
+        assertFalse(menuItemRepository.findById(MENU_ITEM1_ID).isPresent());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getAllForRestaurant() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL)
+                .param("restaurantId", "1"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MENU_ITEM_MATCHER.contentJson(restaurant1_menuitem1, restaurant1_menuitem2,
+                        restaurant1_menuitem3, restaurant1_menuitem4));
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getAllForRestaurantForDate() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL)
+                .param("restaurantId", "1")
+                .param("menuDate", MenuItemTestData.testDate.toString()))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MENU_ITEM_MATCHER.contentJson(restaurant1_menuitem3, restaurant1_menuitem4));
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void createWithLocation() throws Exception {
+        MenuItem newItem = getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newItem)))
+                .andExpect(status().isCreated());
+
+        MenuItem created = MENU_ITEM_MATCHER.readFromJson(action);
+        int newId = created.id();
+        newItem.setId(newId);
+        MENU_ITEM_MATCHER.assertMatch(created, newItem);
+        MENU_ITEM_MATCHER.assertMatch(menuItemRepository.getById(newId), newItem);
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void update() throws Exception {
+        MenuItem updated = getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL + MENU_ITEM1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isNoContent());
+
+        MENU_ITEM_MATCHER.assertMatch(menuItemRepository.getById(MENU_ITEM1_ID), updated);
+    }
+}
